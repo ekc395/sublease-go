97bb911e8174c94749dba4bc4e436ceb5a4bcf4f
ekc395 <ekc395@gmail.com>
2026-03-03 16:49:59 -0800

    Initial commit with mock json data

---
+++ sublease-go/sublease-go/sublease_goApp.swift
@@ -0,0 +1,17 @@
+//
+//  sublease_goApp.swift
+//  sublease-go
+//
+//  Created by Ethan Chen on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct sublease_goApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
